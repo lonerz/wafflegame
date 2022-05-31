cd8751e1503f72315a54375464e0d5cee9d3a4d4
Joshua Pan <joshuapan8@gmail.com>
2022-05-30 22:08:00 -0700

    Wafflegame solver basic strat to get 6 words

---
+++ waffle.py
@@ -0,0 +1,122 @@
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+def colored(color, text):
+  if color == 'green':
+    r, g, b = 0, 255, 0
+  elif color == 'yellow':
+    r, g, b = 255, 255, 0
+  else:
+    r, g, b = 255, 255, 255
+  return "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format(r, g, b, text)
+
+BOARD_SIZE = 5
+NUM_TILES = BOARD_SIZE * BOARD_SIZE - (BOARD_SIZE // 2) * (BOARD_SIZE // 2)
+NUM_WORDS = (BOARD_SIZE // 2 + 1) * 2
+
+driver = webdriver.Chrome()
+driver.get('https://wafflegame.net/')
+
+# close the popup
+# time.sleep(2)
+elem = driver.find_element(by=By.CLASS_NAME, value='button--close')
+try:
+  elem.click()
+except:
+  print('no popup found at this time')
+
+tiles = driver.find_elements(by=By.CSS_SELECTOR, value='.tile.draggable')
+spaces = driver.find_elements(by=By.CLASS_NAME, value='space')
+
+if not tiles or not spaces:
+  print('No tiles or spaces found')
+  driver.quit()
+  exit()
+
+
+class Tile():
+  def __init__(self, letter, pos, class_attr):
+    self.letter = letter
+    coords = json.loads(pos)
+    self.x = coords['x']
+    self.y = coords['y']
+    self.index = coords['x'] + coords['y'] * BOARD_SIZE
+    class_attrs = class_attr.split(' ')
+    if 'tile--' in class_attrs[-1]:
+      self.color = 'black'
+    else:
+      self.color = class_attrs[-1]
+
+  def __str__(self):
+    return colored(self.color, self.letter)
+
+
+class Word():
+  def __init__(self, letters):
+    self.letters = letters
+
+  def __str__(self):
+    word = ''
+    for letter in self.letters:
+      word += str(letter)
+    return word
+
+'''
+Example board is like the following:
+
+  A B C D E
+  F _ G _ H
+  I J K L M
+  N _ O _ P
+  Q R S T U
+
+words are enumerated rows first and then columns
+word 1: ABCDE
+word 2: IJKLM
+word 3: QRSTU
+word 4: AFINQ
+word 5: CGKOS
+word 6: EHMPU
+'''
+
+class Board():
+  def __init__(self, tiles):
+    self.board = [None] * (BOARD_SIZE * BOARD_SIZE)
+    for tile in tiles:
+      t = Tile(tile.text, tile.get_attribute('data-pos'), tile.get_attribute('class'))
+      self.board[t.index] = t
+
+    self.words = [None] * NUM_WORDS
+    for i in range(0, NUM_WORDS, 2):
+      letters = []
+      for j in range(BOARD_SIZE):
+        letters.append(self.board[i * BOARD_SIZE + j])
+      self.words[i // 2] = Word(letters)
+    for i in range(0, NUM_WORDS, 2):
+      letters = []
+      for j in range(BOARD_SIZE):
+        letters.append(self.board[j * BOARD_SIZE + i])
+      self.words[i // 2 + NUM_WORDS // 2] = Word(letters)
+  
+assert(len(tiles) == NUM_TILES)
+assert(len(spaces) == NUM_TILES)
+
+board = Board(tiles)
+
+for word in board.words:
+  print(word)
+
+'''
+for space in spaces:
+  print(space.get_attribute('data-pos'))
+
+time.sleep(1)
+action_chains = ActionChains(driver)
+action_chains.drag_and_drop(tiles[1], spaces[1]).perform()
+'''
+
+time.sleep(10)
+driver.quit()
---
+++ waffle_strat.py
@@ -0,0 +1,313 @@
+from typing import Dict, List, Optional, Set, Tuple
+
+word_list = []
+
+with open('words.txt') as f:
+  for word in f.readlines():
+    word_list.append(word.strip().upper())
+
+
+tiles = [
+  ("C", 0, 0, 0, "green"),
+  ("L", 1, 0, 1, "black"),
+  ("E", 2, 0, 2, "yellow"),
+  ("M", 3, 0, 3, "black"),
+  ("H", 4, 0, 4, "green"),
+  ("A", 0, 1, 5, "yellow"),
+  ("V", 2, 1, 7, "yellow"),
+  ("S", 4, 1, 9, "yellow"),
+  ("S", 0, 2, 10, "yellow"),
+  ("S", 1, 2, 11, "black"),
+  ("I", 2, 2, 12, "green"),
+  ("S", 3, 2, 13, "green"),
+  ("E", 4, 2, 14, "black"),
+  ("U", 0, 3, 15, "black"),
+  ("R", 2, 3, 17, "black"),
+  ("A", 4, 3, 19, "black"),
+  ("S", 0, 4, 20, "green"),
+  ("D", 1, 4, 21, "yellow"),
+  ("K", 2, 4, 22, "black"),
+  ("R", 3, 4, 23, "black"),
+  ("Y", 4, 4, 24, "green"),
+]
+
+
+tiles = [
+  ("M", 0, 0, 0, "green"),
+  ("R", 1, 0, 1, "black"),
+  ("T", 2, 0, 2, "yellow"),
+  ("E", 3, 0, 3, "yellow"),
+  ("Y", 4, 0, 4, "green"),
+  ("B", 0, 1, 5, "black"),
+  ("D", 2, 1, 7, "black"),
+  ("E", 4, 1, 9, "black"),
+  ("E", 0, 2, 10, "yellow"),
+  ("N", 1, 2, 11, "black"),
+  ("O", 2, 2, 12, "green"),
+  ("I", 3, 2, 13, "black"),
+  ("E", 4, 2, 14, "green"),
+  ("L", 0, 3, 15, "black"),
+  ("D", 2, 3, 17, "green"),
+  ("R", 4, 3, 19, "black"),
+  ("T", 0, 4, 20, "green"),
+  ("N", 1, 4, 21, "yellow"),
+  ("A", 2, 4, 22, "yellow"),
+  ("A", 3, 4, 23, "black"),
+  ("D", 4, 4, 24, "green"),
+]
+
+
+def colored(color : str, text : str) -> str:
+  if color == 'green':
+    r, g, b = 0, 255, 0
+  elif color == 'yellow':
+    r, g, b = 255, 255, 0
+  else:
+    r, g, b = 255, 255, 255
+  return "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format(r, g, b, text)
+
+
+BOARD_SIZE = 5
+NUM_TILES = BOARD_SIZE * BOARD_SIZE - (BOARD_SIZE // 2) * (BOARD_SIZE // 2)
+NUM_WORDS = (BOARD_SIZE // 2 + 1) * 2
+
+
+class Tile():
+  def __init__(self, letter : str, x : int, y : int, index : int, color : str) -> None:
+    self.letter = letter
+    self.x = x
+    self.y = y
+    self.index = index
+    self.color = color
+    self.words : List[Word] = []
+    self.space : Optional[Space] = None
+
+  def __str__(self) -> str:
+    return colored(self.color, self.letter)
+
+
+class Space():
+  def __init__(self, index : int, possible_letters : Set[str]) -> None:
+    self.index = index
+    self.possible_letters = possible_letters
+    self.impossible_letters = set()
+    self.tile : Optional[Tile] = None
+
+  def add_impossible(self, letter : str) -> None:
+    if letter in self.possible_letters:
+      self.possible_letters.remove(letter)
+    self.impossible_letters.add(letter)
+
+  def add_impossibles(self, letters : Set[str]) -> None:
+    self.possible_letters.difference_update(letters)
+    self.impossible_letters = self.impossible_letters.union(letters)
+
+  def __str__(self) -> str:
+    return 'Possible letters for space {}: '.format(self.index) + ' '.join(sorted(list(self.possible_letters)))
+
+
+class Word():
+  def __init__(self, letters : List[Tile]) -> None:
+    self.letters = letters
+    self.known_letters : List[str] = []
+    self.possible_answers : List[str] = []
+
+  def __str__(self) -> str:
+    word = ''
+    for letter in self.letters:
+      word += str(letter)
+    return word
+
+
+class Board():
+  def __init__(self, tiles : List[Tuple[str, int, int, int, str]]) -> None:
+    self.board : List[Optional[Tile]] = [None] * (BOARD_SIZE * BOARD_SIZE)
+    all_possible_letters : Set[str] = set()
+    self.letter_to_tile : Dict[str, List[Tile]] = {}
+    for text, x, y, index, color in tiles:
+      t = Tile(text, x, y, index, color)
+      self.board[t.index] = t
+      all_possible_letters.add(t.letter)
+      if t.letter not in self.letter_to_tile:
+        self.letter_to_tile[t.letter] = []
+      self.letter_to_tile[t.letter].append(t)
+    
+    print('all possible letters: ', ' '.join(sorted(list(all_possible_letters))))
+    print()
+
+    self.spaces : List[Optional[Space]] = [None] * (BOARD_SIZE * BOARD_SIZE)
+    for i in range(BOARD_SIZE):
+      for j in range(BOARD_SIZE):
+        if i % 2 and j % 2:
+          continue
+        idx = i * BOARD_SIZE + j
+        self.spaces[idx] = Space(idx, all_possible_letters.copy())
+        self.board[idx].space = self.spaces[idx]
+        self.spaces[idx].tile = self.board[idx]
+
+    self.words : List[Optional[Word]] = [None] * NUM_WORDS
+    for i in range(0, NUM_WORDS, 2):
+      letters : List[Tile] = []
+      for j in range(BOARD_SIZE):
+        letters.append(self.board[i * BOARD_SIZE + j])
+      word = Word(letters)
+      self.words[i // 2] = word
+      for letter in letters:
+        letter.words.append(word)
+    for i in range(0, NUM_WORDS, 2):
+      letters = []
+      for j in range(BOARD_SIZE):
+        letters.append(self.board[j * BOARD_SIZE + i])
+      word = Word(letters)
+      self.words[i // 2 + NUM_WORDS // 2] = word
+      for letter in letters:
+        letter.words.append(word)
+    
+  def print_board(self):
+    for i in range(BOARD_SIZE):
+      for j in range(BOARD_SIZE):
+        if self.board[i * BOARD_SIZE + j]:
+          print(self.board[i * BOARD_SIZE + j], end='')
+        else:
+          print(' ', end='')
+      print()
+
+  
+board = Board(tiles)
+
+board.print_board()
+
+def solve(tile : Tile) -> None:
+  space = tile.space
+  if tile.color == 'green':
+    space.add_impossibles(space.possible_letters.difference(set([tile.letter])))
+    for word in tile.words:
+      word.known_letters.append(tile.letter)
+    if all([t.color == 'green' for t in board.letter_to_tile[tile.letter]]):
+      for t in board.board:
+        if t and t.letter != tile.letter:
+          t.space.add_impossible(tile.letter)
+    return
+  elif tile.color == 'yellow':
+    space.add_impossible(tile.letter)
+    if tile.x % 2 or tile.y % 2:
+      assert(len(tile.words) == 1)
+      tile.words[0].known_letters.append(tile.letter)
+  else:
+    space.add_impossible(tile.letter)
+
+  for word in tile.words:
+    for letter in word.letters:
+      if letter == tile:
+        continue
+      if letter.letter == tile.letter and tile.color != 'green':
+        return
+
+  # Remove black letter from all of its words
+  if tile.color == 'black':
+    for word in tile.words:
+      for letter in word.letters:
+        letter.space.add_impossible(tile.letter)
+
+  # Remove yellow letters from all letters NOT in its words if it's the only one
+  # on the whole board...
+  elif tile.color == 'yellow':
+    tiles = board.letter_to_tile[tile.letter]
+    if len(tiles) == 1:
+      current_letters = set()
+      for word in tile.words:
+        for letter in word.letters:
+          current_letters.add(letter)
+      for t in board.board:
+        if t and t not in current_letters and t.color != 'green':
+          t.space.add_impossible(tile.letter)
+
+def apply_tile_solve(func):
+  for i in range(BOARD_SIZE):
+    for j in range(BOARD_SIZE):
+      if i % 2 and j % 2:
+        continue
+      idx = i * BOARD_SIZE + j
+      func(board.board[idx])
+
+def print_possible_answers():
+  print()
+  apply_tile_solve(lambda t: print(t.space))
+
+apply_tile_solve(solve)
+print_possible_answers()
+
+for word in board.words:
+  print(word)
+
+  for real_word in word_list:
+    possible = True
+    for letter in word.known_letters:
+      if letter not in real_word:
+        possible = False
+        break
+    
+    if not possible:
+      continue
+
+    union_possible_letters = set()
+    for i, tile in enumerate(word.letters):
+      if real_word[i] not in tile.space.possible_letters:
+        possible = False
+        break
+
+    if possible:
+      print('Possible: ', real_word)
+      word.possible_answers.append(real_word)
+
+def is_valid_permutation(words : List[str]) -> bool:
+  # make sure the word intersections match up
+  for i in range(NUM_WORDS // 2):
+    for j in range(NUM_WORDS // 2, NUM_WORDS):
+      col_idx = 2 * (j - NUM_WORDS // 2)
+      if words[i][col_idx] != words[j][2 * i]:
+        return False
+  # make sure the letter counts are the same
+  letter_count : Dict[str, int] = {}
+  for word in words:
+    for letter in word:
+      if letter not in letter_count:
+        letter_count[letter] = 0
+      letter_count[letter] += 1
+  # make sure to account for overcount of the intersection letters
+  for i in range(NUM_WORDS // 2):
+    for j in range(0, NUM_WORDS, 2):
+      letter_count[words[i][j]] -= 1
+  for letter, count in letter_count.items():
+    if count != len(board.letter_to_tile[letter]):
+      return False
+  for letter, tiles in board.letter_to_tile.items():
+    if len(tiles) != letter_count[letter]:
+      return False
+  return True
+
+answer : List[List[str]] = []
+
+cur_idxs = [0] * NUM_WORDS
+while True:
+  cur_words : List[str] = []
+  for i, word in enumerate(board.words):
+    cur_words.append(word.possible_answers[cur_idxs[i]])
+
+  # check if the current permutation of possible answers is valid
+  print(cur_words)
+  if is_valid_permutation(cur_words):
+    print('Valid permutation: ', cur_words)
+    answer = cur_words
+
+  # iterate to next permutation of possible answers
+  cur_idxs[-1] += 1
+  for i in range(NUM_WORDS - 1, 0, -1):
+    if cur_idxs[i] == len(board.words[i].possible_answers):
+      cur_idxs[i] = 0
+      cur_idxs[i - 1] += 1
+  if cur_idxs[0] == len(board.words[0].possible_answers):
+    break
+
+if not answer:
+  print('No valid answer found')
